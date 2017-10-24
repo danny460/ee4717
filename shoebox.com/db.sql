aaed10ba54f92aacf6d8fa9b6e6699967e95fded
Dannyzlh <Danielzlh62@gmail.com>
2017-10-24 17:19:49 +0800

    add login files

--- shoebox.com/db.sql
+++ shoebox.com/db.sql
@@ -1,27 +1,48 @@
 USE shoebox;
 
 CREATE TABLE products (
-    product_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name: VARCHAR(20) NOT NULL,
-    description: VARCHAR(50),
-    brand: VARCHAR(20) NOT NULL,
-    gender: ENUM('men','women','kids') NOT NULL,
-    price: FLOAT(6,2) NOT NULL,
+    product_id SMALLINT UNSIGNED AUTO_INCREMENT,
+    product_name VARCHAR(20) NOT NULL,
+    description VARCHAR(200),
+    brand VARCHAR(20) NOT NULL,
+    gender ENUM('men','women','kids') NOT NULL,
+    price FLOAT(6,2) NOT NULL,
+    CONSTRAINT pk_product_id PRIMARY KEY (product_id)
 );
 
+INSERT INTO products (product_name, description, brand, gender, price) VALUES
+("Adidas NMD 500", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Adidas", "men", 100),
+("Nike HyperX 300", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Nike", "men", 100),
+("New Balance 520", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "New Balance", "men", 250),
+("Puma 250", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "women", 109),
+("Puma 300", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "women", 109),
+("Puma 880", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "kid", 69),
+("Puma 666", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "men", 109);
+
 CREATE TABLE product_variants (
-    product_variant_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    size: TINYINT UNSIGNED NOT NULL,
-    color: VARCHAR(20) NOT NULL,
-    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products,
+    product_variant_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id SMALLINT UNSIGNED NOT NULL,
+    size TINYINT UNSIGNED NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
+
+INSERT INTO product_variants (product_id, size, color) VALUES 
+(1, 42, "Black"),
+(1, 35, "Black"),
+(1, 36, "Black"),
+(1, 42, "Red"),
+(1, 39, "Red"),
+(1, 42, "Blue"),
+(1, 42, "White");
 
 CREATE TABLE users (
     user_id: VARCHAR(36) NOT NULL PRIMARY KEY,
-    username: VARCHAR(16),
-    email: VARCHAR(50),
-    hash: VARCHAR(256),
+    username: VARCHAR(16) NOT NULL,
+    email: VARCHAR(50) NOT NULL,
+    hash: VARCHAR(64) NOT NULL,
+    CONSTRAINT UNIQUE KEY uk_username
+    CONSTRAINT UNIQUE KEY uk_email
 );
 
 CREATE TABLE cart_item (
---
+++ shoebox.com/public_html/include/login.php
@@ -0,0 +1,13 @@
+<?php 
+    function login($username, $password){
+
+    }
+
+    function signup($username, $email, $password){
+        
+    }
+
+    function hash_SHA256($plaintext){
+        return hash("sha256", $plaintext);
+    }
+?>
