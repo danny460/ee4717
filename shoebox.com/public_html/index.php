01472dfad9dfb2201d7294fa2a473e1a76101b0a
Unknown <gzhang008@e.ntu.edu.sg>
2017-10-26 23:08:02 +0800

    stylize product page
    Still need to add php and  js inside

--- shoebox.com/public_html/index.php
+++ shoebox.com/public_html/index.php
@@ -12,7 +12,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="/js/script.js"></script>
     <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>THE SHOEBOX</title>
 </head>
 
--- shoebox.com/public_html/product.php
+++ shoebox.com/public_html/product.php
@@ -22,6 +22,44 @@
             <?php include 'partials/nav.php'?>
         </header>    
     </main>
+
+        <div class="product-left-wrapper">
+            <div class="product-image-wrapper">
+                <img src="/assets/products/air-jordan-1-retro-high-flyknit-shoe.jpg" alt="product name"></img>
+            </div>
+        </div>
+
+        <div class = "product-right-wrapper">
+            <div class="product-title-wrapper">
+                <h2>product name</h2>
+            </div>
+            <div class="product-title-wrapper">
+                <h4>$ 100.0</h4>
+            </div>
+            <div class="product-title-wrapper">
+                <h4>COLOR: white/red/black</h4>
+            </div
+            <div class="product-title-wrapper">
+                <h5>Color:</h5>
+                <form action="">
+                  <input type="radio" name="color" value="white"> white
+                  <input type="radio" name="color" value="red"> red
+                  <input type="radio" name="color" value="black"> black
+                </form>
+                <h5>Size:</h5>
+                <form action="">
+                   <input type="radio" name="size" value="39"> 39
+                  <input type="radio" name="size" value="40"> 40
+                  <input type="radio" name="size" value="41"> 41
+                  <input type="radio" name="size" value="42"> 42
+                   <input type="radio" name="size" value="43"> 43
+                </form>
+                <label for="quantity">Quantity: </label>
+                <input type="number" id="quantity" style="width:90px;"/><br><pre>
+                <input type = "submit"  value = "Add to cart" />
+            </div>
+        </div>
+
     <?php
         include "partials/cart.php";
         include "partials/footer.php";
