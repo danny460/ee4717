72791a9b766c3585be9d653e6fbda6b3285dd7c4
Dannyzlh <Danielzlh62@gmail.com>
2017-10-28 21:41:28 +0800

    fix login and add signup

--- shoebox.com/db.sql
+++ shoebox.com/db.sql
@@ -11,6 +11,12 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, description, brand, gender, price) VALUES
+("Puma 1250", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "men", 109),
+("Adidas NMD FX", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Adidas", "men", 100),
+("Nike Runner", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Nike", "men", 100),
+("Puma 1250", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "women", 109),
+("Puma 3300", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "women", 109),
+("Puma 3666", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "men", 109),
 ("Adidas NMD 500", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Adidas", "men", 100),
 ("Nike HyperX 300", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Nike", "men", 100),
 ("New Balance 520", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "New Balance", "men", 250),
@@ -18,6 +24,7 @@ INSERT INTO products (product_name, description, brand, gender, price) VALUES
 ("Puma 300", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "women", 109),
 ("Puma 880", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "kid", 69),
 ("Puma 666", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "Puma", "men", 109);
+
 
 CREATE TABLE product_variants (
     product_variant_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -28,22 +35,27 @@ CREATE TABLE product_variants (
 );
 
 INSERT INTO product_variants (product_id, size, color) VALUES 
-(1, 42, "Black"),
 (1, 35, "Black"),
 (1, 36, "Black"),
-(1, 42, "Red"),
+(1, 37, "Black"),
+(1, 38, "Red"),
 (1, 39, "Red"),
-(1, 42, "Blue"),
-(1, 42, "White");
+(1, 40, "Blue"),
+(1, 41, "White");
 
 CREATE TABLE users (
-    user_id: VARCHAR(36) NOT NULL PRIMARY KEY,
-    username: VARCHAR(16) NOT NULL,
-    email: VARCHAR(50) NOT NULL,
-    hash: VARCHAR(64) NOT NULL,
-    CONSTRAINT UNIQUE KEY uk_username
-    CONSTRAINT UNIQUE KEY uk_email
+    user_id VARCHAR(36) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id),
+    CONSTRAINT uc_username UNIQUE (username),
+    CONSTRAINT uc_email UNIQUE (email)
 );
+
+/*
+    NOT VALID BELOW
+*/
 
 CREATE TABLE cart_item (
     item_id: INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -53,42 +65,4 @@ CREATE TABLE cart_item (
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users,
     CONSTRAINT fk_product_variant_id FOREIGN KEY (product_variant_id) REFERENCES product_variants
 );
-
-
--- CREATE TABLE order (
---     order_id
--- );
-
--- CREATE TABLE order_item (
---     order_item_id
--- );
-
-
--- CREATE TABLE variants (
---     variant_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
---     variant_name: VARCHAR(15) NOT NULL,
--- );
-
--- CREATE TABLE variant_values (
---     value_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     product_id: SMALLINT UNSIGNED,
---     variant_name: VARCHAR(15),
---     value: VARCHAR(15),
---     CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products
--- );
-
--- CREATE TABLE product_variants (
---     product_variant_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     product_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
---     product_variant_name: VARCHAR(50),
---     CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products,
--- );
-
--- CREATE TABLE product_variant_details (
---     detail_id: SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     product_variant_id: SMALLINT UNSIGNED,
---     value_id: SMALLINT UNSIGNED,
---     CONSTRAINT fk_product_variant_id FOREIGN KEY (product_variant_id) REFERENCES product_variants,
---     CONSTRAINT fk_value_id FOREIGN KEY (value_id) REFERENCES variant_values
--- );
 
