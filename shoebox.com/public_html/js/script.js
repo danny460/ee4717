3455b95606ed8e55b24c4f58803f139ed3d86fa5
Dannyzlh <Danielzlh62@gmail.com>
2017-11-03 00:14:31 +0800

    editing cart page

--- shoebox.com/public_html/js/script.js
+++ shoebox.com/public_html/js/script.js
@@ -1,11 +1,16 @@
-// window.onload = function(){
-    function openNav() {
-        document.getElementById("cartSidenav").style.width = "250px";
-        // document.getElementById("main").style.marginRight = "250px";   
-    }
-    /* Set the width of the side navigation to 0 */
-    function closeNav() {
-        document.getElementById("cartSidenav").style.width = "0";
-        // document.getElementById("main").style.marginRight = "0";  
-    }
-// };
+function openNav() {
+    var sidenav = document.getElementById("cartSidenav");
+    sidenav.style.right = "0px";
+    sidenav.className += " open";
+}
+function closeNav() {
+    var sidenav = document.getElementById("cartSidenav");
+    sidenav.style.right = "-" +sidenav.clientWidth+"px";
+    sidenav.className = "cart-side-nav";
+}
+
+//test
+setTimeout(function(){
+    openNav();
+}, 200);
+
---
+++ shoebox.com/public_html/partials/item-edit.php
@@ -0,0 +1,5 @@
+<?php
+    if(isset($_REQUEST["item_id"])){
+        echo '<div id="response"><p>TEST '.$_REQUEST["item_id"].'</p></div>';
+    }
+?>
