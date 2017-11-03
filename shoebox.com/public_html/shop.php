646bf30378fdbf588dfb376be466f083868ff7f0
Dannyzlh <Danielzlh62@gmail.com>
2017-11-03 16:23:39 +0800

    add basic filter

--- shoebox.com/public_html/shop.php
+++ shoebox.com/public_html/shop.php
@@ -2,14 +2,44 @@
     session_start();
     include_once("./include/db.php");
     $mysqli = db_connect();
+    print_r($_POST);
+    print(count($_POST["colors"]));
+    // gender filter
     $for = $_GET["for"];
     if ($for == null) {
         $for = "men";
     }
+    // products
+    $products = null;
     $stmt = "SELECT * FROM products WHERE gender = '".$for."';";
     $brands_stmt = "SELECT DISTINCT brand FROM products";
     $color_stmt = "SELECT DISTINCT color FROM product_variants";
-    $products = $mysqli->query($stmt);
+    // filter
+    if (isset($_POST["reset"])){
+        unset($_POST["brands"]);
+        unset($_POST["colors"]);
+    } if (isset($_POST["filter"])){
+        $filterStmt = "SELECT * from products WHERE gender = '$for'";
+        if(isset($_POST["colors"])){
+            if(count($_POST["colors"])){
+                $colors = join("','", $_POST["colors"]);
+                $filterStmt = $filterStmt." AND product_id IN (SELECT product_id FROM product_variants WHERE color in ('$colors'))";
+            }
+        }
+        if(isset($_POST["brands"])){
+            if(count($_POST["brands"])){
+                $brands = join("','", $_POST["brands"]);
+                $filterStmt = $filterStmt." AND brand IN ('$brands')";
+            }
+        }
+        $filterStmt = $filterStmt.";";
+        echo '<h1>QUERY: '.$filterStmt.'</h1>';
+        $products = $mysqli->query($filterStmt);
+    }else{
+        $products = $mysqli->query($stmt);
+    }
+    
+    
     $brands = $mysqli->query($brands_stmt);
     $colors = $mysqli->query($color_stmt);
 ?>
@@ -29,7 +59,13 @@
         <div class="container shop-container">
             <div class="filter-column-container">
                 <div class="filter-container" id="filter">
+                    <?php
+                        echo '<form action="/shop.php?for='.$for.'" method="post">';
+                    ?>                        
+                    <input class="btn btn-secondary float-right inline" type="submit" value="apply" name="filter">
+                    <input class="btn btn-secondary float-right inline" style="margin-right:15px;" type="submit" value="reset" name="reset">
                     <h1 class="headline">Filters</h2>
+                    
                     <span class="keyline-horizontal keyline-grey"></span>
                     <h3>Brand</h3>
                     <!-- <span class="keyline-horizontal keyline-grey"></span> -->
@@ -39,7 +75,15 @@
                                 
                                 while($brand = $brands->fetch_assoc()){
                                     echo '
-                                        <div class="check-box-wrapper"><label><input type="checkbox">'.$brand["brand"].'</label></div>
+                                        <div class="check-box-wrapper"><label><input type="checkbox" name="brands[]" value="'.$brand["brand"].'"
+                                    ';
+                                    if(isset($_POST["brands"])){
+                                        if(in_array($brand["brand"], $_POST["brands"])){
+                                            echo ' checked ';
+                                        }
+                                    }
+                                    echo '
+                                        >'.$brand["brand"].'</label></div>
                                     ';
                                 }
                             }
@@ -52,20 +96,32 @@
                             if($colors->num_rows > 0){
                                 while($color = $colors->fetch_assoc()){
                                     echo '
-                                        <div class="color-cube bg-'.$color['color'].'"></div>
+                                        <div class="selection-cube bg-'.$color['color'].'">
+                                            <label>
+                                                <input type="checkbox" value="'.$color['color'].'" name="colors[]"
+                                    ';
+                                    if(isset($_POST["colors"])){
+                                        if(in_array($color["color"], $_POST["colors"])){
+                                            echo ' checked ';
+                                        }
+                                    }           
+                                    echo '
+                                        ><span>&#x2714;</span>
+                                            </label>
+                                        </div>
                                     ';
                                 }
                             }
                         ?>
                     </div>    
                     <div class="filter-section" id="filter-price">
-                            
                     </div>        
+                    </form>
                 </div>
             </div>
             <div class="product-list-container" id="product-list">
                 <?php 
-                    if($products->num_rows > 0){
+                    if($products != null && $products->num_rows > 0){
                         while($item = $products->fetch_assoc()){
                             echo '   
                                 <div class="product-grid-wrapper" onclick="navToDetailPage(\''.$item["product_id"].'\')">
@@ -83,7 +139,7 @@
                         }
                     }else{
                         echo '
-                            <h2>No matching content</h2>
+                            <h3 style="width:100%;text-align:center;padding:50px 0;">No matching content</h3>
                         ';
                     }
                 ?>
