ee26fcda5c3032c9bd5fb44fe386d39c77126ea9
Dannyzlh <Danielzlh62@gmail.com>
2017-11-10 00:44:34 +0800

    edited footer

--- shoebox.com/public_html/partials/footer.php
+++ shoebox.com/public_html/partials/footer.php
@@ -1,14 +1,11 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <footer>
     <div id="footer">
-            <!-- <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-facebook"></a>
             <a href="#" class="fa fa-twitter"></a>
-            <a href="#" class="fa fa-google"></a>
-            <a href="#" class="fa fa-youtube"></a>
-            <a href="#" class="fa fa-instagram"></a><br> -->
-            <p><b>Personal Project</b></p>
-            Copyright &copy; 2017 EE4717 Assignment - 'shoebox' group<br>
-            Group member: Zhang LinHan  Zhang Guobin<br><br>
-            <small>DISCLOSURE: Welcome to our online shop page. This is a school web project and we work as team of two members. It last for one semester.:)  Thanks for your support!</small>
+            <a href="#" class="fa fa-instagram"></a><br>
+            <p><b>Follow us to get new product updates</b></p>
+            <p class="hint-txt">&copy; SHOEBOX - EE4717 F37-DG15<br></p>
+            <p class="hint-txt">Group member: Zhang Linghan  Zhang Guobin<br></p>
     </div>
 </footer>
